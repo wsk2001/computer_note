e024d228abbab69021d27b3f577a1f9754f75c9c
wsk2001 <wsk2001@gmail.com>
2018-10-19 12:05:35 +0900

    C++ Command line parser test program
    C++ Command line parser test program

---
+++ Develop/C/CommandLineParserTest.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "CommandLineParser.hpp"
+
+void setOption(CmdLineParser * parser)
+{
+    parser->addOption("-h","help");
+    parser->addOption("-conf","Load config file");
+    parser->addOption("-sleep","Sleep milliseconds, default value is '0'");
+    parser->addOption("-diskLimit","Free disk space, limit(MBytes, ex: 500)");
+
+    parser->addOption("-ll","Write log level, default values is 'WARNING'\n\t\t\t"
+                     "Available options are 'TRACE', 'DEBUG', 'INFO', 'WARNING', 'ERROR', and 'FATAL'.");
+
+}
+
+int main(int argc, char **argv)
+{ 
+    CmdLineParser cmdparams;
+
+    cmdparams.load(argc, argv);
+    setOption( &cmdparams );
+
+    cmdparams.setUsage(argv[0],"-conf sc_enc.conf -sleep 10 -ll ERROR");
+
+    if( cmdparams.validationCheck() == false )
+    {
+        cmdparams.printOptions();
+        exit( 0 );
+    }
+      
+	if( cmdparams.cmdOptionExists("-h")  || 
+        cmdparams.cmdOptionExists("--h") || 
+        cmdparams.cmdOptionExists("/h")  ||
+        cmdparams.cmdOptionExists("/?") 
+        )
+	{
+        cmdparams.printOptions();
+        exit( 0 );
+    }
+    
+    if( argc < 2 )
+    {
+        cmdparams.printOptions();
+        exit( 0 );
+    }
+
+	const std::string &log_level = cmdparams.getCmdOption("-ll");
+    if (!log_level.empty()){
+        printf( "log level = %s\n", log_level.c_str() );
+    }
+    
+	return 0;
+}
+
+/*
+ compile: g++ -o CommandLineParserTest CommandLineParserTest.cpp
+ run test: CommandLineParserTest -ll DEBUG
+*/
